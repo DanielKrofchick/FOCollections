a6a79b2b1da2ede883af9ae1cf8c35fd18fca09b
Daniel Krofchick <krofchick@gmail.com>
2016-02-23 16:46:42 -0500

    Refactored resource retrieval.

--- FOCollections/UICollectionView/FOCollectionItem.swift
+++ FOCollections/UICollectionView/FOCollectionItem.swift
@@ -19,9 +19,27 @@ public class FOCollectionItem: NSObject, UICollectionViewDelegateFlowLayout {
     public weak var viewController: UIViewController? = nil                        // Weak reference to viewController
     
     public func configure(cell: UICollectionViewCell, collectionView: UICollectionView, indexPath: NSIndexPath){}
-    public func getExtras(collectionView: UICollectionView, indexPath: NSIndexPath){}
-    public func setExtras(collectionView: UICollectionView, indexPath: NSIndexPath, extras: [NSObject: AnyObject]){}
+    // Gets the reource identified by key
+    public func getResource(forKey key: String, collectionView: UICollectionView, indexPath: NSIndexPath, completion: ((resource: AnyObject, result: AnyObject) -> ())) -> NSOperation? {return nil}
+    // Populates the cell with the resource identified by key. IndexPath may have changed with async call, do not rely on it.
+    public func setResource(resource: AnyObject, result: AnyObject, forKey key: String, collectionView: UICollectionView, indexPath: NSIndexPath){}
+    // The item produces a unique key per resource
+    public func resourceKeys() -> [String]{return [String]()}
     
+    public func getResources(collectionView: UICollectionView, indexPath: NSIndexPath) -> [NSOperation] {
+        var operations = [NSOperation]()
+        
+        for key in resourceKeys() {
+            if let operation = getResource(forKey: key, collectionView: collectionView, indexPath: indexPath, completion: {[weak self] (resource, result) -> () in
+                self?.setResource(resource, result: result, forKey: key, collectionView: collectionView, indexPath: indexPath)
+            }) {
+                operations.append(operation)
+            }
+        }
+        
+        return operations
+    }
+
     override public func isEqual(o: AnyObject?) -> Bool {
         if let o = o as? FOCollectionItem {
             if o.data == nil && data == nil {
--- FOCollections/UITableView/FOTableItem.swift
+++ FOCollections/UITableView/FOTableItem.swift
@@ -18,8 +18,26 @@ public class FOTableItem: NSObject, UITableViewDelegate, UITableViewDataSource {
     weak public var viewController: UIViewController? = nil
     
     public func configure(cell: UITableViewCell, tableView: UITableView, indexPath: NSIndexPath){}
-    public func getExtras(tableView: UITableView, indexPath: NSIndexPath){}
-    public func setExtras(tableView: UITableView, indexPath: NSIndexPath, extras: [NSObject: AnyObject]){}
+    // Gets the reource identified by key
+    public func getResource(forKey key: String, tableView: UITableView, indexPath: NSIndexPath, completion: ((resource: AnyObject, result: AnyObject) -> ())) -> NSOperation? {return nil}
+    // Populates the cell with the resource identified by key. IndexPath may have changed with async call, do not rely on it.
+    public func setResource(resource: AnyObject, result: AnyObject, forKey key: String, tableView: UITableView, indexPath: NSIndexPath){}
+    // The item produces a unique key per resource
+    public func resourceKeys() -> [String]{return [String]()}
+    
+    public func getResources(tableView: UITableView, indexPath: NSIndexPath) -> [NSOperation] {
+        var operations = [NSOperation]()
+        
+        for key in resourceKeys() {
+            if let operation = getResource(forKey: key, tableView: tableView, indexPath: indexPath, completion: {[weak self] (resource, result) -> () in
+                self?.setResource(resource, result: result, forKey: key, tableView: tableView, indexPath: indexPath)
+            }) {
+                operations.append(operation)
+            }
+        }
+        
+        return operations
+    }
     
     override public func isEqual(o: AnyObject?) -> Bool {
         if let o = o as? FOTableItem {
